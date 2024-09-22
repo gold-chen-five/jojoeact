16f1a1d3782a13cf1183b640d9a7e99a529c613f
jojo <rachel7465022@gmail.com>
2024-09-23 02:41:32 +0800

    working on useEffect ...

--- src/example/App.tsx
+++ src/example/App.tsx
@@ -4,16 +4,25 @@ import { Test } from './Test';
 
 const App = () => {
   const [count, setCount] = useState<number>(0);
+  const [object, setObject] = useState<{test: string}>({test: 'hello'});
 
   // Define a handler for the button click
   function handleClick() {
     setCount((prev) => prev+1);
   }
 
+  function handleOnChange(e: Event){
+    const target = e.target as HTMLInputElement;  
+    const value = target.value;
+    setObject({test: value})
+  }
+
   return (
     <div className="my-component">
       <h1>main</h1>
       <button onclick={handleClick}>Test</button>
+      <input oninput={handleOnChange}/>
+      <p>{object.test}</p>
       <Test count={count}/>
     </div>
   );
---
+++ src/hooks/useEffect.ts
@@ -0,0 +1,3 @@
+export function useEffect(callback: () => void, deps: any[]){
+
+}
